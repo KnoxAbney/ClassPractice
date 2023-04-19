a4226ed86a8b89bc43907c477266223571fb39c7
KnoxAbney <110506819+KnoxAbney@users.noreply.github.com>
2023-04-19 10:38:21 -0400

    Update Profile.cpp
    restarted from scratch and completed a few basic methods.

--- Profile.cpp
+++ Profile.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 #include "profile.hpp"
 
-  Profile::Profile(std::string nname, int nage, std::string ncity, std::string ncountry, std::string npronouns, std::string nhobby)
-  : name(nname), age(nage), city(ncity), country(ncountry), pronouns(npronouns), hobbies(nhobby){}
-
-  void Profile::getProfile(){
-
-    std::cout << "Your current profile:\n\n";
-    std::cout << "Name: " << name << "\n";
-    std::cout << "Age: " << age << "\n";
-    std::cout << "City: " << city << ", " << country << "\n";
-    std::cout << "Pronouns: " << pronouns << "/n";
-    std::cout << "Hobbies: " << hobbies << "/n";
-
+Profile::Profile(std::string nname, int nage, std::string ncity, std::string ncountry, std::string npronouns)
+  : name(nname), age(nage), city(ncity), country(ncountry), pronouns(npronouns){
+    
   }
 
-  std::string Profile::addHobby(std::string nhobby){
+  std::string Profile::viewProfile(){
+    std::string output = "User name: " + name + "\n";
+    output += "User age: " + std::to_string(age) + "\n";
+    output += "User area: " + city + ", " + country + "\n";
+    output += "User pronouns: " + pronouns + "\n";
 
-    hobbies = hobbies.append("\n");
-    hobbies = hobbies.append(nhobby);
+    output += "User hobbies: ";
+    for (int i=0; i < hobbies.size(); i++){
+      if(i == hobbies.size()-1){
+        output += hobbies[i] + "\n";
+      }else{
+      output += hobbies[i] + ", ";
+      }
+    }
 
+    return output;
   }
 
-
-
+  void Profile::newHobby(std::string nhobby){
+    hobbies.push_back(nhobby);
+  }
